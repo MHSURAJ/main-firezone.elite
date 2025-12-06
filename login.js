78aebd0b12d6b8c9f9baec841d6a75635964bf21
MHSURAJ <surushannu@gmail.com>
2025-12-06 20:41:10 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,40 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { 
-    getAuth, 
-    GoogleAuthProvider, 
-    signInWithPopup,
-    onAuthStateChanged 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+function googleLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD66aIVOTVco6rTtpVinkl64UZGnZDgn1o",
-  authDomain: "firezone-elite.firebaseapp.com",
-  databaseURL: "https://firezone-elite-default-rtdb.firebaseio.com",
-  projectId: "firezone-elite",
-  storageBucket: "firezone-elite.firebasestorage.app",
-  messagingSenderId: "121224317328",
-  appId: "1:121224317328:web:a4d19bab51fc8076400b15",
-  measurementId: "G-9YR0LYN37X"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-// -------- Google Login Button --------
-document.getElementById("googleLogin").onclick = function() {
-    signInWithPopup(auth, provider)
-        .then(() => {
+    firebase.auth().signInWithPopup(provider)
+        .then((result) => {
+            localStorage.setItem("user", JSON.stringify(result.user));
             window.location.href = "public.html";
         })
-        .catch(err => {
-            alert("Error: " + err.message);
+        .catch((error) => {
+            alert("Error: " + error.message);
         });
-};
-
-// -------- If already logged in, auto redirect --------
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        window.location.href = "public.html";
-    }
-});
+}
