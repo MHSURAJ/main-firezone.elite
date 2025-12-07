fc3744de58b8abbea6ab3d4f88efeed8172e404d
MHSURAJ <surushannu@gmail.com>
2025-12-07 10:24:22 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,33 @@
-const adminEmail = "surushannu@gmail.com";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+    getAuth, GoogleAuthProvider, signInWithPopup 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+    getDatabase, ref, get, set
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-function googleLogin() {
-    var provider = new firebase.auth.GoogleAuthProvider();
+const firebaseConfig = {
+  apiKey: "AIzaSyD66aIVOTVco6rTtpVinkl64UZGnZDgn1o",
+  authDomain: "firezone-elite.firebaseapp.com",
+  databaseURL: "https://firezone-elite-default-rtdb.firebaseio.com",
+  projectId: "firezone-elite",
+  storageBucket: "firezone-elite.firebasestorage.app",
+  messagingSenderId: "121224317328",
+  appId: "1:121224317328:web:a4d19bab51fc8076400b15",
+  measurementId: "G-9YR0LYN37X"
+};
 
-    firebase.auth().signInWithPopup(provider)
-        .then((result) => {
-            let user = result.user;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+const provider = new GoogleAuthProvider();
+
+document.getElementById("googleBtn").addEventListener("click", () => {
+    signInWithPopup(auth, provider)
+        .then(async (result) => {
+            const user = result.user;
+
+            const adminEmail = "surushannu@gmail.com";
 
             if (user.email === adminEmail) {
                 window.location.href = "admin.html";
@@ -13,7 +35,8 @@ function googleLogin() {
                 window.location.href = "public.html";
             }
         })
+
         .catch((error) => {
-            alert("Login Error: " + error.message);
+            alert("Login Failed: " + error.message);
         });
-}
+});
